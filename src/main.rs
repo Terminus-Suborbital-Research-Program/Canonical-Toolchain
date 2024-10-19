1859dfd6e5bc4e0b9660366221ddc84a6c96942b
Lucas Thelen <thelenlucas028@gmail.com>
2024-10-19 15:01:44 -0500

    Change supported processor to RP2350

--- src/main.rs
+++ src/main.rs
@@ -4,67 +4,68 @@
 
 use panic_halt as _;
 
-#[rtic::app(device = rp_pico::hal::pac)]
-mod app {
-    use fugit::MicrosDurationU32;
+// Alias for our HAL crate
+use rp235x_hal as hal;
 
-    use embedded_hal::digital::OutputPin;
+// Some things we need
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
 
-    use rp2040_hal::fugit::Duration;
-    use rp2040_hal::timer::Alarm;
-    use rp2040_hal::timer::CountDown;
-    use rp_pico::hal;
-    use rp_pico::pac;
+/// Tell the Boot ROM about our application
+#[link_section = ".start_block"]
+#[used]
+pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
 
-    use rp_pico::XOSC_CRYSTAL_FREQ; // Frequency of the crystal on the board
+/// External high-speed crystal on the Raspberry Pi Pico 2 board is 12 MHz.
+/// Adjust if your board has a different frequency
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
-    // Blink every 5 seconds
-    const BLINK_TIME_US: u32 = 5_000_000;
+/// Entry point to our bare-metal application.
+///
+/// The `#[hal::entry]` macro ensures the Cortex-M start-up code calls this function
+/// as soon as all global variables and the spinlock are initialised.
+///
+/// The function configures the rp235x peripherals, then toggles a GPIO pin in
+/// an infinite loop. If there is an LED connected to that pin, it will blink.
+#[hal::entry]
+fn main() -> ! {
+    // Grab our singleton objects
+    let mut pac = hal::pac::Peripherals::take().unwrap();
 
-    #[shared]
-    struct Shared {
-        timer: hal::Timer,
-        alarm: hal::timer::Alarm0,
-        led_on: bool,
-        led_pin: rp2040_hal::gpio::Pin<rp2040_hal::gpio::bank0::Gpio25, rp2040_hal::gpio::FunctionSio<rp2040_hal::gpio::SioOutput>, rp2040_hal::gpio::PullDown>,
-    }
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    #[local]
-    struct Local {}
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
 
-    #[init]
-    fn init(c: init::Context) -> (Shared, Local) {
-        let mut resets =  c.device.RESETS;
-        let mut watchdog = hal::watchdog::Watchdog::new(c.device.WATCHDOG);
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
 
-        let clocks = hal::clocks::init_clocks_and_plls(
-            XOSC_CRYSTAL_FREQ, 
-            c.device.XOSC, 
-            c.device.CLOCKS,
-            c.device.PLL_SYS,
-            c.device.PLL_USB,
-            &mut resets,
-            &mut watchdog,
-        ).ok().unwrap();
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
 
-        // GPIO init
-        let sio = hal::Sio::new(c.device.SIO);
-        let pins = rp_pico::Pins::new(c.device.IO_BANK0, c.device.PADS_BANK0, sio.gpio_bank0, &mut resets);
-        let mut led = pins.led.into_push_pull_output();
-        led.set_low().unwrap();
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
 
-        // Timer init
-        let mut timer = hal::Timer::new(c.device.TIMER, &mut resets, &clocks);
-        let mut alarm = timer.alarm_0().unwrap();
-        let duration = MicrosDurationU32::micros(BLINK_TIME_US);
-        alarm.schedule(duration).unwrap();
-
-        (Shared {
-            timer,
-            alarm,
-            led_on: false,
-            led_pin: led,
-        }, Local {})
-        
+    // Configure GPIO25 as an output
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+    loop {
+        led_pin.set_high().unwrap();
+        timer.delay_ms(500);
+        led_pin.set_low().unwrap();
+        timer.delay_ms(500);
     }
 }
