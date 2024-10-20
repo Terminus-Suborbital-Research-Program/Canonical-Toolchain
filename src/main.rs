1f7c0606e73e3be819fce82e834a1803ee9ffe70
Lucas Thelen <thelenlucas028@gmail.com>
2024-10-20 14:46:30 -0500

    RP235X Compat

--- src/main.rs
+++ src/main.rs
@@ -4,68 +4,70 @@
 
 use panic_halt as _;
 
-// Alias for our HAL crate
-use rp235x_hal as hal;
+// You would not beleive how long this took to figure out
+use rtic_monotonics::rp235x::prelude::*;
 
-// Some things we need
-use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::OutputPin;
+rp235x_timer_monotonic!(Mono);
 
 /// Tell the Boot ROM about our application
 #[link_section = ".start_block"]
 #[used]
-pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
+pub static IMAGE_DEF: rp235x_hal::block::ImageDef = rp235x_hal::block::ImageDef::secure_exe();
 
-/// External high-speed crystal on the Raspberry Pi Pico 2 board is 12 MHz.
-/// Adjust if your board has a different frequency
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+#[rtic::app(
+    device = rp235x_hal::pac
+)]
+mod app {
+    use super::*;
+    use embedded_hal::digital::{OutputPin, StatefulOutputPin};
+    use rp235x_hal::{
+        clocks, gpio::{
+            self, bank0::Gpio25, FunctionSio, PullDown, PullNone, SioOutput
+        }, pac, sio::Sio, timer::Timer, watchdog::Watchdog
+    };
 
-/// Entry point to our bare-metal application.
-///
-/// The `#[hal::entry]` macro ensures the Cortex-M start-up code calls this function
-/// as soon as all global variables and the spinlock are initialised.
-///
-/// The function configures the rp235x peripherals, then toggles a GPIO pin in
-/// an infinite loop. If there is an LED connected to that pin, it will blink.
-#[hal::entry]
-fn main() -> ! {
-    // Grab our singleton objects
-    let mut pac = hal::pac::Peripherals::take().unwrap();
+    use fugit::RateExtU32;
 
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
-    // Configure the clocks
-    let clocks = hal::clocks::init_clocks_and_plls(
-        XTAL_FREQ_HZ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .unwrap();
+    #[shared]
+    struct Shared {}
 
-    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+    #[local]
+    struct Local {
+        led: gpio::Pin<Gpio25, FunctionSio<SioOutput>, PullNone>
+    }
 
-    // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local) {
+        let mut watchdog = Watchdog::new(ctx.device.WATCHDOG);
 
-    // Set the pins to their default state
-    let pins = hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
+        Mono::start(ctx.device.TIMER0, &ctx.device.RESETS);
 
-    // Configure GPIO25 as an output
-    let mut led_pin = pins.gpio25.into_push_pull_output();
-    loop {
-        led_pin.set_high().unwrap();
-        timer.delay_ms(500);
+        // The single-cycle I/O block controls our GPIO pins
+        let sio = Sio::new(ctx.device.SIO);
+
+        // Set the pins to their default state
+        let pins = rp235x_hal::gpio::Pins::new(
+            ctx.device.IO_BANK0,
+            ctx.device.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut ctx.device.RESETS,
+        );
+
+        // Configure GPIO25 as an output
+        let mut led_pin = pins.gpio25.into_pull_type::<PullNone>().into_push_pull_output();
         led_pin.set_low().unwrap();
-        timer.delay_ms(500);
+        blink::spawn().ok();
+        (Shared {}, Local {led: led_pin})
+    }
+
+    #[task(local = [led])]
+    async fn blink(ctx: blink::Context) {
+        loop {
+            _ = ctx.local.led.toggle();
+
+            Mono::delay(1000.millis()).await;
+        }
     }
 }
